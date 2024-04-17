e35d4f1808b5eeb6c49d4400441d8b609d096f9d
tomperg <pt7063@mci4me.at>
2024-04-17 11:19:25 +0200

    small change

---
+++ main.py
@@ -0,0 +1,13 @@
+from load_data import load_data
+from sort import bubble_sort
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = load_data("activity.csv")
+power_W = data['PowerOriginal']
+#Sort
+sorted_power_W = bubble_sort(power_W)
+
+
+print(data)
+print(sorted_power_W[::-1])
