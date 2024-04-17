139b0bbec1d8a19879db2844166d3bf7ca24bf56
tomperg <pt7063@mci4me.at>
2024-04-17 11:46:20 +0200

    deleted

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sort import bubble_sort
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = load_data("activity.csv")
+data = load_data('activity.csv')
 power_W = data['PowerOriginal']
 #Sort
 sorted_power_W = bubble_sort(power_W)
